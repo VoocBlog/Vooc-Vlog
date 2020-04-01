8efab00a5c1fcfba8c31385da09d5a0e19adae0d
toannq06031999 <toannq06031999@gmail.com>
2020-04-01 21:38:16 +0700

    Update 3
    Thêm vào page chính

---
+++ Vooc@3_Test_1/js/post-test.js
@@ -0,0 +1,9 @@
+	function show_post(){
+	    $("#text-area").css("display","none");
+	    $("#abc").fadeIn();
+	}
+	function hide_post(){
+	    $("#abc").css("display","none");
+	    $("#text-area").fadeIn();
+	}
+
