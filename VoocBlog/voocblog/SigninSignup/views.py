319a4773e77cefa94466477cd00d94d738f0613c
phatntt1999 <phatntt1999@gmail.com>
2020-04-06 00:57:06 +0700

    SignUp function with verify username and password, but without notificate for end-user

--- VoocBlog/voocblog/SigninSignup/views.py
+++ VoocBlog/voocblog/SigninSignup/views.py
@@ -7,6 +7,9 @@ from django.http import HttpResponseRedirect
 
 from django import forms
 
+from . import ValidateRegister
+import logging
+
 
 # Create your views here.
 def index(request):
@@ -15,9 +18,9 @@ def index(request):
 # def login(request):
 #     return render(request, 'LoginRegister/sign-in.html')
 
-def signup(request):
-    return render(request, 'LoginRegister/sign-up.html')
 
+def error(request):
+    return render(request, 'pages/error.html')
 
 def login(request):
     if request.method == 'POST':
@@ -46,6 +49,33 @@ def login(request):
         #     # raise forms.ValidationError('Invalid password')
         #     return render(request, 'LoginRegister/sign-up.html')
     return render(request, 'LoginRegister/sign-in.html')
+    
+def signup(request):
+    if request.method == 'POST':
+        enterFName = request.POST['firstName']
+        enterLName = request.POST['lastName']
+        enterEmail = request.POST['username']
+        enterPassword = request.POST['password']
+        enterConfirmPassword = request.POST['confirmPassword']
+
+
+        if ValidateRegister.Validate_Username(enterEmail):
+            if ValidateRegister.Validate_Password(enterPassword, enterConfirmPassword):
+                ValidateRegister.save(enterEmail, enterPassword, enterFName, enterLName)
+                return HttpResponseRedirect('/')
+                #return logging.info("Correct")
+            else:
+                logging.debug('Password not match')
+                return render(request, "pages/error.html")
+        else:
+            return HttpResponseRedirect('error')
+
+
+        
+            
+        
+    return render(request, "LoginRegister/sign-up.html")
+
 
 def profile(request):
     if request.session.has_key('username'):
@@ -61,3 +91,4 @@ def logout(request):
     except:
         pass
     return render(request, 'LoginRegister/sign-in.html')
+
