2fd9f1c417f44ec61664254d2694f81cdadeffde
toannq06031999 <toannq06031999@gmail.com>
2020-04-07 16:32:03 +0700

    Update 4
    Edit code and folder

--- Vooc@3_Test_1/js/post-test.js
+++
@@ -1,9 +0,0 @@
-	function show_post(){
-	    $("#text-area").css("display","none");
-	    $("#abc").fadeIn();
-	}
-	function hide_post(){
-	    $("#abc").css("display","none");
-	    $("#text-area").fadeIn();
-	}
-
